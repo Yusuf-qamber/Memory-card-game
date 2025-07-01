823a6680b473fda2222954e188ff3b8ad93a0e4f
Yusuf qamber <yousif02.qamber@gmail.com>
2025-07-01 23:27:33 +0300

    adding html

---
+++ js/app.js
@@ -0,0 +1,25 @@
+/*-------------- Constants -------------*/
+
+
+/*---------- Variables (state) ---------*/
+
+
+/*----- Cached Element References  -----*/
+const cards=document.querySelectorAll(".card")
+console.log(cards)
+/*-------------- Functions -------------*/
+function init(){
+
+}
+
+function render(){
+
+}
+
+function shuffle(arr){
+  
+
+}
+
+
+/*----------- Event Listeners ----------*/
